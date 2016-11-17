cb19327d73d79dc0f163e618b2014fe4a5fca811
ZTF <464599233@qq.com>
2016-11-17 10:47:08 +0800

    新增加地址页面

---
+++ app/src/main/java/dq/lelaohui/com/lelaohuipad/fragement/shop/MyAddressActivity.java
@@ -0,0 +1,32 @@
+package dq.lelaohui.com.lelaohuipad.fragement.shop;
+
+import android.os.Bundle;
+
+import dq.lelaohui.com.lelaohuipad.base.LeLaoHuiBaseActivity;
+import dq.lelaohui.com.lelaohuipad.port.IControler;
+
+/**
+ * Created by ZTF on 2016/11/17.
+ */
+
+public class MyAddressActivity extends LeLaoHuiBaseActivity {
+    @Override
+    public IControler getControler() {
+        return null;
+    }
+
+    @Override
+    public void result(Bundle bundle) {
+
+    }
+
+    @Override
+    protected int getLayoutID() {
+        return 0;
+    }
+
+    @Override
+    public void usable() {
+
+    }
+}
