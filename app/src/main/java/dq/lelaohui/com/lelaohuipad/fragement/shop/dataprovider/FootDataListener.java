79035a0b96d622d1e4f3eda6c04e73091dae83ab
quda <chengyuruo@jd.com>
2016-12-14 16:07:09 +0800

    订餐功能优化

---
+++ app/src/main/java/dq/lelaohui/com/lelaohuipad/fragement/shop/dataprovider/FootDataListener.java
@@ -0,0 +1,9 @@
+package dq.lelaohui.com.lelaohuipad.fragement.shop.dataprovider;
+
+/**
+ * Created by ThinkPad on 2016/12/14.
+ */
+
+public interface FootDataListener {
+    void dataChanager(String id);
+}
